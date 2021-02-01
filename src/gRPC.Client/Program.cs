c902c02d7067fb141a94ee9e58832e6839647c20
Henrique Andrade <henrique.anndrade@hotmail.com>
2021-01-31 21:00:58 -0300

    Add project files.

---
+++ src/gRPC.Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Grpc.Core;
+using gRPC.Model;
+
+namespace gRPC.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel("localhost:30051", ChannelCredentials.Insecure);
+
+            var client = new Greeter.GreeterClient(channel);
+
+            string nome = "";
+
+            do
+            {
+                Console.WriteLine("\nDiga seu nome:");
+                nome = Console.ReadLine();
+                try
+                {
+                    var reply = client.SayHello(new HelloRequest { Name = nome });
+                    Console.WriteLine($"[SERVER] {reply.Message}");
+                }
+                catch (Grpc.Core.RpcException e)
+                {
+                    Console.WriteLine($"[Error] STATUS_CODE: {e.Status.StatusCode} » {e.Status.Detail}");
+                }
+            } while (nome != "sair");
+
+            channel.ShutdownAsync().Wait();
+        }
+    }
+}
---
+++ src/gRPC.Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using gRPC.Model;
+using Grpc.Core;
+
+namespace gRPC.Server
+{
+    class Program
+    {
+        const int port = 30051;
+
+        static void Main(string[] args)
+        {
+            Grpc.Core.Server server = new Grpc.Core.Server
+            {
+                Services = { Greeter.BindService(new GreeterImpl()) },
+                Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Server is now listening on port: " + port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+
+    class GreeterImpl : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"[CLIENT] {request.Name}");
+
+            return Task.FromResult(new HelloReply { Message = $"Olá {request.Name}, espero que tenho um ótimo dia!" });
+        }
+    }
+}
